92c2e8af9ddc981d07d35d083476a0de40fee487
seonb2n <626ksb@gmail.com>
2022-07-21 21:05:17 +0900

    QueryDsl 을 사용한 검색 기능 구현

--- src/main/java/com/example/board/repository/ArticleRepository.java
+++ src/main/java/com/example/board/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package com.example.board.repository;
 
 import com.example.board.domain.Article;
+import com.example.board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.hibernate.criterion.SimpleExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository
+        extends JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        //listing 하지 않은 property 는 검색에서 제외시킨다.
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.hashtag, root.createdAt, root.createdBy);
+        //내용을 포함하고 있는 식으로 검색한다.
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
